1c48dc7c2c04cbb04092ab9022cd3978f974076a
yusukeendou <yusuke20023@gmail.com>
2017-07-22 16:59:32 +0900

    supported wallpaper

---
+++ app.js
@@ -0,0 +1,24 @@
+var electron = require("electron");
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("ready", function() {
+  mainWindow = new BrowserWindow({
+    width: 1680,
+    height: 1050,
+    type: "desktop",
+    frame: false
+  });
+  mainWindow.setPosition(0, 0);
+  mainWindow.loadURL("file://" + __dirname + "/index.html");
+  
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
+});
